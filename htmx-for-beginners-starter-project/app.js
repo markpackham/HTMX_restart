5e9f6d91a8a39d8953fed767ce581a2e124d4699
mark <markpackham1@gmail.com>
2024-08-02 17:25:33 +0100

    Add edit view page
    https://youtu.be/3slYTHP6c-8?si=aL_6RTSqTFNnRG8H&t=37

--- htmx-for-beginners-starter-project/app.js
+++ htmx-for-beginners-starter-project/app.js
@@ -3,6 +3,7 @@ import BOOKS_DATA from "./data/data.js";
 import createHomepageTemplate from "./views/index.js";
 import createListTemplate from "./views/list.js";
 import createBookTemplate from "./views/book.js";
+import createEditFormTemplate from "./views/edit.js";
 
 // create app
 const app = express();
@@ -48,6 +49,12 @@ app.delete("/books/:id", (req, res) => {
   // it is fine if we send an empty response
   // we want to replace the inner html of the Delete button with nothing
   res.send();
+});
+
+app.get("/books/edit/:id", (req, res) => {
+  const book = BOOKS_DATA.find((b) => b.id === req.params.id);
+
+  res.send(createEditFormTemplate(book));
 });
 
 // listen to port
